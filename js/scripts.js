ec7cfe281293a98f3aef2dae93cae904bba36865
Dziarnek <kuba.dziarnowski@onet.eu>
2017-10-31 23:08:02 +0100

    Creating project

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var treeLayers = 0;
+
+function drawTree(layers) {
+    for (var i = 0 ; i < layers ; i++ ) {
+        var star = "*";
+        for (var j = 0 ; j < i ; j++) {
+            star += "*";
+        };
+        console.log(star);
+    };
+};
+
+treeLayers = prompt("Ile warstw ma mieć drzewko?");
+    drawTree(treeLayers);
+
